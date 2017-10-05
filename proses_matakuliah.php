8766ec839b8751f47eb72661ccdb31f2df8a2524
nicolauslawe <nicolauslawe@gmail.com>
2017-10-05 17:40:31 +0700

    Delete Matakuliah

--- proses_matakuliah.php
+++ proses_matakuliah.php
@@ -11,7 +11,10 @@
 		//2.Query
 		$query = "UPDATE matakuliah SET kode_matkul = '$_POST[kode_matkul]', nama_matkul = '$_POST[nama_matkul]' WHERE id=$_POST[id]";
 	}
-
+	else if($_GET['action'] == "delete")
+	{
+		$query = "DELETE FROM matakuliah WHERE id =$_GET[id]";
+	}
 
 	mysqli_query ($koneksi, $query);
 	
