c39f7aacdff5b1d630b94454567845074c0f77aa
nicolauslawe <nicolauslawe@gmail.com>
2017-10-05 16:47:05 +0700

    Mahasiswa
    Berdasarkan PDF yang diberikan oleh Bapak Alex.

---
+++ dosen.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<h1>Data Dosen</h1>
+<table>
+	<thead>
+		<tr>
+			<th>No.</th>
+			<th>Kode Dosen</th>
+			<th>Nama Dosen</th>
+		</tr>
+	</thead>
+	<tbody>
+			<th>1</th>
+			<th>L90</th>
+			<th>Hencet Unyu</th>
+	</tbody>
+</table>
