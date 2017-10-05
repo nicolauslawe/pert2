d4828ef44760d71ae88e16e3ea162b5759eb6cc0
nicolauslawe <nicolauslawe@gmail.com>
2017-10-05 17:40:02 +0700

    Update Matakuliah

--- proses_matakuliah.php
+++ proses_matakuliah.php
@@ -6,6 +6,12 @@
 		//2. Query
 		$query ="INSERT INTO matakuliah (kode_matkul, nama_matkul) VALUES ('$_POST[kode_matkul]', '$_POST[nama_matkul]')";
 	}
+	else if($_GET['action'] == "edit")
+	{
+		//2.Query
+		$query = "UPDATE matakuliah SET kode_matkul = '$_POST[kode_matkul]', nama_matkul = '$_POST[nama_matkul]' WHERE id=$_POST[id]";
+	}
+
 
 	mysqli_query ($koneksi, $query);
 	
